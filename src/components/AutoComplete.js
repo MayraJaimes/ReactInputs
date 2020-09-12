38cd2c4da6a7b56f0df36213458c3b2770f5c92d
Mayra Jaimes <mtj26@georgetown.edu>
2020-09-12 16:20:00 -0500

    added the option list

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -60,9 +60,8 @@ export class Autocomplete extends Component {
     }
 
     }
+    
 
-
-  
   render(){
     const{
       onChange,
@@ -73,6 +72,31 @@ export class Autocomplete extends Component {
       }
     } = this;
     let optionList;
+
+    if(showOptions && userInput){
+      if(filteredOptions.length){
+        optionList = (
+          <ul className='options'> 
+          {filteredOptions.map(() => {
+            let className;
+            if(index === activeOption){
+              className = 'option-active';
+            }
+            return(
+              <li className={className} key={optionName} onClick={onClick}>
+              {optionName}
+              </li>
+            );
+          })}
+          </ul>
+        );
+      }
+    }
+    else {
+      optionList = (
+        <div className='no-options'> <em>No Option!</em></div>
+      )
+    }
     return (
       <React.Fragment>
       <div className="search">
