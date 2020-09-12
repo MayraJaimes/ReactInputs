a41b982bb2a6c8f8c4609c9648f40f5477f26673
Mayra Jaimes <mtj26@georgetown.edu>
2020-09-12 16:12:13 -0500

    added the events

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,12 +1,84 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Autocomplete extends Component {
+  static propTypes ={
+    options:PropTypes.instanceOf(Array).isRequired
+  }
+  state = {
+    activeOption: 0,
+    filteredOptions: [],
+    showOptions: false,
+    userInput: ''
+  }
+
+  onChange = (e) => {
+    const {options} = this.props;
+    const userInput = e.currentTarget.value;
+    const filteredOptions = options.filter(
+      (option) => option.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+    );
+    this.setState({
+      activeOption: 0, 
+      filteredOptions, 
+      showOptions:true,
+      userInput
+    })
+  }
+
+  onClick = (e) => {
+    this.setState({
+      activeOption: 0,
+      filteredOption: 0,
+      showOptions:false,
+      userInput: e.currentTarget.innerText
+    })
+  }
+
+  onKeyDown = (e) => {
+    const {activeOption, filteredOptions } = this.state;
+
+    if(e.keyCode === 13){
+      this.setState({
+        activeOption: 0,
+        showSuggestions: false,
+        userInput: filteredOptions[activeOption]
+      })
+    }
+    else if(e.keyCode ===38) {
+      if(activeOption ===0){
+        return;
+      }
+      this.setState({activeOption: activeOption -1});
+    }
+
+    else if(e.keyCode === 40){
+      if(activeOption -1 === filteredOptions.length){
+        return;
+      }
+      this.setState({activeOption: activeOption + 1});
+    }
+
+    }
+
+
+  
   render(){
+    const{
+      onChange,
+      onChange,
+      onKeyDown,
+      userInput,
+      state: {activeOption, filteredOption, showOptions, userInput
+      }
+    } = this;
+    let optionList;
     return (
       <React.Fragment>
       <div className="search">
-      <input type='text' className='search-box' />
-      <input type='submit' value='' className='search-btn' />
+        <input type='text' className='search-box' onChange={onChange} onKeyDown={onKeyDown} value={userInput}/>
+        <input type='submit' value='' className='search-btn' />
+        {optionList}
       </div>
       </React.Fragment>
 
